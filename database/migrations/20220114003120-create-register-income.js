0a1eb49078cfaf4d09ac78e56f89cebe6c09d40a
Mozart <mozartrueckert@outlook.com>
2022-01-14 20:37:22 -0300

    fix: migrations columns

--- database/migrations/20220114003120-create-register-income.js
+++ database/migrations/20220114003120-create-register-income.js
@@ -33,11 +33,11 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      created_at: {
+      createdAt: {
         type: Sequelize.DATE,
         allowNull: false,
       },
-      updated_at: {
+      updatedAt: {
         type: Sequelize.DATE,
         allowNull: false,
       },
