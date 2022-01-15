85c199d802b5bd432a5b132b8abd32fea10963c8
Mozart <mozartrueckert@outlook.com>
2022-01-14 22:10:57 -0300

    feat: added routes related to Account model and 'contas' table

--- app/controllers/AccountController.js
+++ app/controllers/AccountController.js
@@ -1,12 +1,10 @@
-const { contas } = require("../models");
+const { contas, sequelize } = require("../models");
 
 module.exports = {
   // POST -> Create a new account
   async create_account(req, res) {
-    // get data from request
     const { nome, saldo, tipo_conta, inst_financeira } = req.body;
 
-    // Insert Values into the database
     const user = await contas.create({
       nome,
       saldo,
@@ -15,5 +13,54 @@ module.exports = {
     });
 
     return res.json(user);
+  },
+
+  // PUT -> Edit an account
+  async edit_account(req, res) {
+    const account_id = req.params.id;
+
+    const { nome, tipo_conta, inst_financeira } = req.body;
+
+    await contas.update(
+      {
+        nome: `${nome}`,
+        tipo_conta: `${tipo_conta}`,
+        inst_financeira: `${inst_financeira}`,
+      },
+      {
+        where: { conta: `${account_id}` },
+      }
+    );
+
+    return res.send(`The account ${account_id} has been updated`);
+  },
+
+  // DELETE -> Remove an account
+  async delete_account(req, res) {
+    const account_id = req.params.id;
+
+    await contas.destroy({
+      where: { conta: `${account_id}` },
+    });
+
+    return res.send(`Account ${account_id} has been deleted`);
+  },
+
+  // GET -> List all registered accounts
+  async list_accounts(req, res) {
+    const accounts = await contas.findAll();
+
+    return res.json(accounts);
+  },
+
+  // GET -> List total Bank Balance
+  async total_bank_balance(req, res) {
+    const total = await contas.findAll({
+      attributes: [
+        [sequelize.fn("sum", sequelize.col("saldo")), "saldo_total"],
+      ],
+    });
+
+    return res.json(total);
   },
 };
--- routes.js
+++ routes.js
@@ -8,6 +8,18 @@ const AccountController = require("./app/controllers/AccountController");
 // Create a new account
 routes.post("/create_account", AccountController.create_account);
 
+// Edit an account
+routes.put("/edit_account/:id", AccountController.edit_account);
+
+// Remove an account
+routes.delete("/delete_account/:id", AccountController.delete_account);
+
+// List accounts
+routes.get("/get_accounts", AccountController.list_accounts);
+
+// List total bank balance
+routes.get("/get_total_balance", AccountController.total_bank_balance);
+
 // --------------------------------------------- //
 
 module.exports = routes;
