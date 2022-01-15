6043c53a8df2e67f1a357946bede0cf36c005347
Mozart <mozartrueckert@outlook.com>
2022-01-15 15:21:03 -0300

    feat: add hooks configuration

--- app/models/Incomes.js
+++ app/models/Incomes.js
@@ -1,20 +1,63 @@
 const { sequelize, DataTypes } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-  const receitas = sequelize.define("receitas", {
-    receita_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: true,
+  const receitas = sequelize.define(
+    "receitas",
+    {
+      receita_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      conta: DataTypes.INTEGER,
+      valor: DataTypes.FLOAT,
+      data_recebimento: DataTypes.DATEONLY,
+      data_recebimento_esperado: DataTypes.DATEONLY,
+      descricao: DataTypes.STRING,
+      tipo_receita: DataTypes.STRING,
     },
-    conta: DataTypes.INTEGER,
-    valor: DataTypes.FLOAT,
-    data_recebimento: DataTypes.DATEONLY,
-    data_recebimento_esperado: DataTypes.DATEONLY,
-    descricao: DataTypes.STRING,
-    tipo_receita: DataTypes.STRING,
-  });
+    {
+      hooks: {
+        afterCreate: async (receita) => {
+          await sequelize.models.contas.update(
+            {
+              saldo: sequelize.literal(`saldo + ${receita.valor}`),
+            },
+            { where: { conta: receita.conta } }
+          );
+        },
+        beforeDestroy: async (receita) => {
+          await sequelize.models.contas.update(
+            {
+              saldo: sequelize.literal(`saldo - ${receita.valor}`),
+            },
+            { where: { conta: receita.conta } }
+          );
+        },
+        beforeUpdate: async (receita) => {
+          await sequelize.models.contas.update(
+            {
+              saldo: sequelize.literal(
+                `saldo - ${receita._previousDataValues.valor}`
+              ),
+            },
+            {
+              where: { conta: receita.conta },
+            }
+          );
+        },
+        afterUpdate: async (receita) => {
+          await sequelize.models.contas.update(
+            {
+              saldo: sequelize.literal(`saldo + ${receita.valor}`),
+            },
+            { where: { conta: receita.conta } }
+          );
+        },
+      },
+    }
+  );
 
   return receitas;
 };
